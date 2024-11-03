8ae42973e24b2be818fb07c1bf3593fb7e00713f
Ted <23tpehanich@gmail.com>
2024-11-03 08:59:47 -0800

    More bug fixes.

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from vex import *
 
 brain=Brain()
 
-right_wheel_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1)
-right_wheel_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1)
-left_wheel_1 = Motor(Ports.PORT3, GearSetting.RATIO_18_1)
-left_wheel_2 = Motor(Ports.PORT4, GearSetting.RATIO_18_1)
+right_wheel_1 = Motor(Ports.PORT1, GearSetting.RATIO_6_1)
+right_wheel_2 = Motor(Ports.PORT2, GearSetting.RATIO_6_1)
+left_wheel_1 = Motor(Ports.PORT3, GearSetting.RATIO_6_1)
+left_wheel_2 = Motor(Ports.PORT4, GearSetting.RATIO_6_1)
 
 right_wheels = MotorGroup (right_wheel_1, right_wheel_2)
 left_wheels = MotorGroup (left_wheel_1, left_wheel_2)
@@ -26,16 +26,16 @@ def sprint():
     if bradley_controller_joystick_left>2 and bradley_controller.buttonL1.pressing:
         left_wheels.spin(FORWARD, 120, RPM)
     if bradley_controller.buttonL1.released:
-        (left_wheels.stop)
+        left_wheels.stop()
     
     if bradley_controller_joystick_right>2 and bradley_controller.buttonL1.pressing:
         right_wheels.spin(FORWARD, 120, RPM)
     if bradley_controller.buttonL2.released:
-        (right_wheels.stop)
+        right_wheels.stop()
 
 def intake():
     if bradley_controller.buttonB.pressed and flex_wheels.velocity(RPM)>-5 and flex_wheels.velocity(RPM)<5:
-        flex_wheels.spin(FORWARD, RPM, 20)
+        flex_wheels.spin(FORWARD, 20, RPM)
     if bradley_controller.buttonB.pressed and flex_wheels.velocity(RPM)>5 or flex_wheels.velocity(RPM)<-5:
         flex_wheels.stop()
 

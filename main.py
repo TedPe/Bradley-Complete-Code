6891fbaf9195ee7dd670f051f8263c25bf5e5234
Ted <23tpehanich@gmail.com>
2024-11-07 08:00:55 -0800

    Finished pnuematics addition

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ rpm2 = 0
 rpm3 = 0
 rpm4 = 0
 
+goal_lock_piston = DigitalOut(brain.three_wire_port.a)
+goal_lock_piston.set(True)
 
 right_wheel_1 = Motor(Ports.PORT11, GearSetting.RATIO_6_1)
 right_wheel_2 = Motor(Ports.PORT12, GearSetting.RATIO_6_1)
@@ -33,22 +35,22 @@ def sprint():
     if bradley_controller_joystick_left>2 and bradley_controller.buttonL1.pressing:
         global rpm1
         rpm1 += 25
-        max (rpm1, 570)
+        rpm1 = min (rpm1, 570)
     
     if bradley_controller_joystick_right>2 and bradley_controller.buttonL1.pressing:
         global rpm2
         rpm2 += 25
-        max (rpm2, 570)
+        rpm2 = min (rpm2, 570)
 
     if bradley_controller_joystick_left<-2 and bradley_controller.buttonL1.pressing:
         global rpm3
         rpm3 += 25
-        max (rpm3, 570)
+        rpm3 = min (rpm3, 570)
     
     if bradley_controller_joystick_right<-2 and bradley_controller.buttonL1.pressing:
         global rpm4
         rpm4 += 25
-        max (rpm4, 570)
+        rpm4 = min (rpm4, 570)
 
 def intake():
     if bradley_controller.buttonB.pressed and flex_wheels.velocity(RPM)>-5 and flex_wheels.velocity(RPM)<5:
@@ -66,57 +68,74 @@ def drive():
     if bradley_controller_joystick_left>2:
         global rpm1
         rpm1 += 20
-        max (rpm1, 400)
+        rpm1 = min (rpm1, 400)
 
     if bradley_controller_joystick_right>2:
         global rpm2
         rpm2 += 20
-        max (rpm2, 400)
+        rpm2 = min (rpm2, 400)
 
     if bradley_controller_joystick_left<-2:
         global rpm3
         rpm3 += 20
-        max (rpm3, 400)
+        rpm3 = min (rpm3, 400)
     
     if bradley_controller_joystick_right<-2:
         global rpm4
         rpm4 += 20
-        max (rpm4, 400)
+        rpm4 = min (rpm4, 400)
 
 
-    if rpm2 > rpm1:
-        rpm1 -= 50
-        min (rpm1, 0)
-    if rpm1 > rpm2:
-        rpm2 -= 50
-        min (rpm2, 0)
-    if rpm3 > rpm4:
-        rpm4 -= 50
-        min (rpm4, 0)
-    if rpm4 > rpm3:
-        rpm3 -= 50
-        min (rpm3, 0)
+    if rpm3 > rpm1:
+        rpm1 =0
+    if rpm4 > rpm2:
+        rpm2 =0
+    if rpm2 > rpm4:
+        rpm4 =0
+    if rpm1 > rpm3:
+        rpm3 =0
+
+
+    if rpm1 < 0:
+        rpm1 = 0
+    if rpm2 < 0:
+        rpm2 = 0
+    if rpm3 < 0:
+        rpm3 = 0
+    if rpm4 < 0:
+        rpm4 = 0
+
+    if rpm1 < 600:
+        rpm1 = 570
+    if rpm2 < 600:
+        rpm2 = 570
+    if rpm3 < 600:
+        rpm3 = 570
+    if rpm4 < 600:
+        rpm4 = 570
 
 def stop():
-    if bradley_controller_joystick_left >-3 and bradley_controller_joystick_left<3:
+    if bradley_controller_joystick_left >-5 and bradley_controller_joystick_left<5:
         global rpm1
         global rpm3
 
-        rpm1 -= 50
-        rpm3 -= 50
+        rpm1 = 0
+        rpm3 = 0
 
-        min (rpm1, 0)
-        min (rpm3, 0)
-
-    if bradley_controller_joystick_right >-3 and bradley_controller_joystick_right<3:
+    if bradley_controller_joystick_right >-5 and bradley_controller_joystick_right<5:
         global rpm2
         global rpm4
 
-        rpm2 -= 50
-        rpm4 -= 50
+        rpm2 = 0
+        rpm4 = 0
 
-        min (rpm2, 0)
-        min (rpm4, 0)
+def goal_lock():
+    if bradley_controller.buttonR1.pressed:
+        goal_lock_piston.set(False)
+
+    if bradley_controller.buttonR2.pressed:
+        goal_lock_piston.set(True)
+
 
 while True:
    
@@ -125,6 +144,7 @@ while True:
     intake()
     ring_riser()
     stop()
+    goal_lock()
 
     left_wheels.spin(FORWARD, rpm1, RPM)
     right_wheels.spin(FORWARD, rpm2, RPM)
